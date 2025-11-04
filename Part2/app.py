d87626ebd36df60287f890f191b826a7c79c801a
CRB112 <connorrb1111@gmail.com>
2025-11-04 18:03:09 -0500

    Added home page, login, and some navigation

--- Part2/app.py
+++ Part2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, flash, redirect, render_template, request, jsonify, url_for, session
 import config
 import pymysql
 
@@ -7,8 +7,9 @@ import pymysql
 
 db = config.db1
 app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
 
-user = "", "", -1
+user = ("", -1)
 
 
 #Zone to add more
@@ -16,9 +17,44 @@ user = "", "", -1
 
 @app.route('/')
 def returnHome():
-    if user == ("", "", -1):
-        return render_template('login.html')
-    return render_template('index.html', user=user)
+    return render_template('index.html', user=session.get('username'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def loginPage():
+    global user
+    
+    if request.method=='POST':
+        username = request.form.get('loginUser')
+        password = request.form.get('loginPass')
+        
+        print(username, password)
+
+        if not username or not password:
+            flash('Invalid Credentials', 'danger')
+            return redirect(url_for('loginPage'))
+
+        sql = "SELECT username, permission_level FROM users where username=%s AND password=%s"
+        cursor.execute(sql, (username, password,))
+        tempUser = cursor.fetchall()
+
+        if not tempUser:
+            flash('Login not Found', 'danger')
+            return redirect(url_for('loginPage'))
+        
+        session['username'] = tempUser[0][0]
+        session['password'] = tempUser[0][1]
+
+        return redirect(url_for('returnHome'))
+
+    return render_template('login.html', user=user)
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    flash('You have been logged out', 'info')
+    return redirect(url_for('returnHome'))
+
 
 if __name__ == '__main__':
     cursor=db.cursor()
